598a73edab2204110ff72b0866f31edb547512b0
Pkdotts <andybaole1234@gmail.com>
2026-02-16 15:33:46 -0500

    removed seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,25 +10,25 @@ const prisma = new PrismaClient({
   adapter,
 });
 
-const projectData: Prisma.ProjectCreateInput[] = [
-  {
-    projectId: "a-wetter-world",
-    title: "A Wetter World",
-    bannerUrl: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-5.png",
-    description: "Rainy cloud yadadadyadyadyayday",
-  },
-  {
-    projectId: "mother-encore",
-    title: "MOTHER Encore",
-    bannerUrl: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-1.png",
-    description: "That sure was a mother encore",
-  },
-];
+// const projectData: Prisma.ProjectCreateInput[] = [
+//   {
+//     projectId: "a-wetter-world",
+//     title: "A Wetter World",
+//     bannerUrl: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-5.png",
+//     description: "Rainy cloud yadadadyadyadyayday",
+//   },
+//   {
+//     projectId: "mother-encore",
+//     title: "MOTHER Encore",
+//     bannerUrl: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-1.png",
+//     description: "That sure was a mother encore",
+//   },
+// ];
 
 export async function main() {
-  for (const p of projectData) {
-    await prisma.project.create({ data: p });
-  }
+  // for (const p of projectData) {
+  //   await prisma.project.create({ data: p });
+  // }
 }
 
 main();
