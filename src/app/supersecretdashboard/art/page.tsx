0ed0fb20e574ce38aa8df6b81f2a923f6561da08
Pkdotts <andybaole1234@gmail.com>
2026-02-16 19:05:29 -0500

    fixed art page

--- src/app/supersecretdashboard/art/page.tsx
+++ src/app/supersecretdashboard/art/page.tsx
@@ -1,9 +1,8 @@
-"use server"
 import ArtDashboard from "./client";
 import prisma from "@/lib/prisma";
 
 export default async function ArtDashboardPage() {
-  const art = prisma.art.findMany();
+  const art = await prisma.art.findMany();
   return (
     <ArtDashboard art={art}/>
   );
